eec90cfc461f68d982609ff47b4466368bc7e7f7
Pondidum <Pondidum@stormbase.net>
2011-03-24 17:00:50 +0000

    removed unused files, added ncover project

--- Forge.Templating/IReplacementStrategy.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Interface IReplacementStrategy
-
-    Sub Setup(ByVal template() As Char)
-    Sub Replace(ByVal key As String, ByVal value As String)
-    ReadOnly Property Result() As String
-
-End Interface
--- Forge.Templating/IReplacments.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Interface IReplacments
-
-    ReadOnly Property Replacements As IList(Of KeyValuePair(Of String, String))
-
-End Interface
