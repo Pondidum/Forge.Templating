b702924a0f9cf306dd7422c2273ec8afc4aba46c
Pondidum <Pondidum@stormbase.net>
2011-03-26 10:33:10 +0000

    started on tag specs

---
+++ Forge.Templating.Specs/SearchStrategies/Token/Tags/ValueTagSpecs.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+
+namespace Forge.Templating.Specs.SearchStrategies.Token.Tags
+{
+    public class ValueTagSpecs
+    {
+        
+    }
+}
